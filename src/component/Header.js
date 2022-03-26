e32794384ff407d97f1ba2d71603602096bc788d
Andy <andykdhk@gmail.com>
2022-03-25 23:30:11 -0600

    planning

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div class="header">
+      <h1>Restaurant</h1>
+      <div class="navi-bar">
+        <p>Menu</p>
+        <p>Order</p>
+        <p>About</p>
+      </div>
+    </div>
+  );
+}
